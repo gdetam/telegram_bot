a309cb406c5e82785b3c27b97e3f89e0f005a723
gdetam <hellostyle@mail.ru>
2020-11-19 18:45:16 +0300

    feature/_added_update_/models/genre.py

--- models/genre.py
+++ models/genre.py
@@ -1,14 +1,16 @@
 """this is genres table structure."""
 
-from sqlalchemy import Integer, String, Column
-from sqlalchemy.orm import relationship
-
 from models.base import Base
 from models.books_genres import books_genres
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
 
 class Genres(Base):
+    """This is Genres class for table structure."""
+
     __tablename__ = 'genres'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    books = relationship("Books", secondary=books_genres)
+    books = relationship('Books', secondary=books_genres)
