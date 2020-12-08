b22fd5ed3572e4d34a9a32ea482a14f4ed51ce7f
gdetam <hellostyle@mail.ru>
2020-12-08 17:59:57 +0300

    feature/_added_/keyboards/paginations/pagination_genres_keyboards.py

---
+++ keyboards/paginations/pagination_genres_keyboards.py
@@ -0,0 +1,42 @@
+"""this is genre's pagination keyboards creator."""
+
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def add_pagination_for_books_by_genre(
+                                      keyboard: InlineKeyboardMarkup,
+                                      current_page: int,
+                                      count_page: int,
+                                      catch_item_id: int):
+    """Add pagination for books."""
+    keyboard.row(*add_arrow_for_books_by_genre(current_page, count_page,
+                                               catch_item_id))
+    return InlineKeyboardMarkup(keyboard)
+
+
+def add_arrow_for_books_by_genre(
+                                 current_page: int,
+                                 count_page: int,
+                                 catch_item_id: int):
+    """Add buttons to switch to books."""
+    row = []
+    if current_page != 0:
+        row.append(InlineKeyboardButton(
+                                        text='<',
+                                        callback_data='genre_id_book_page_'
+                                                      + str(catch_item_id)
+                                                      + '/'
+                                                      + str(current_page - 1)))
+
+    row.append(InlineKeyboardButton(
+                                    text='в меню',
+                                    callback_data='menu'))
+
+    if current_page != count_page:
+        row.append(InlineKeyboardButton(
+                                        text='>',
+                                        callback_data='genre_id_book_page_'
+                                                      + str(catch_item_id)
+                                                      + '/'
+                                                      + str(current_page + 1)))
+    return row
