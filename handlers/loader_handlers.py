034adc7fc0a2a1f2cc307dcd03f4f52ba85488e8
gdetam <hellostyle@mail.ru>
2020-12-21 18:16:56 +0300

    feature/_added_/handlers/loader_handlers.py

---
+++ handlers/loader_handlers.py
@@ -0,0 +1,8 @@
+"""this is loader for handlers."""
+
+from . import authors_handler
+from . import books_handler
+from . import callback_handler
+from . import genres_handler
+from . import menu_handler
+from . import readers_handler
